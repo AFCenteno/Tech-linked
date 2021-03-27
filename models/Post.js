013df756a3c9fcb9141d2ee99d8af4bff5f255ca
mrafcent1 <alexfcenteno@outlook.com>
2021-03-27 15:53:34 -0400

    update models

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
 // create our Post model
-class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
+class Post extends Model {}
+
+  Post.findOne({
         where: {
           id: body.post_id
         },
@@ -16,7 +13,6 @@ class Post extends Model {
           'post_url',
           'title',
           'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
           {
@@ -28,10 +24,8 @@ class Post extends Model {
             }
           }
         ]
-      });
-    });
-  }
-}
+  });
+
 
 // create fields/columns for Post model
 Post.init(
--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
-const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 // create associations
@@ -15,16 +14,11 @@ Post.belongsTo(User, {
 });
 
 User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
-
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
 });
 
 Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
   foreignKey: 'post_id',
   onDelete: 'SET NULL'
 });
@@ -37,14 +31,6 @@ Vote.belongsTo(User, {
 Vote.belongsTo(Post, {
   foreignKey: 'post_id',
   onDelete: 'SET NULL'
-});
-
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
@@ -66,4 +52,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
