1557b346e329cd20297eb0077717aa021cbd2fd4
mrafcent1 <alexfcenteno@outlook.com>
2021-03-27 23:49:00 -0400

    fix

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,6 +2,7 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  const body = document.querySelector('input[name="post-body"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
@@ -9,7 +10,7 @@ async function editFormHandler(event) {
     method: 'PUT',
     body: JSON.stringify({
       title,
-      post_body
+      body
     }),
     headers: {
       'Content-Type': 'application/json'
