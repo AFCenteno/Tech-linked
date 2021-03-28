6c86bcf93a634dd4c509fefafcea876a61d8c6a4
mrafcent1 <alexfcenteno@outlook.com>
2021-03-28 13:37:01 -0400

    Finalw/timedsignout

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -11,4 +11,25 @@ async function logout() {
   }
 }
 
+var idleTime = 0;
+  $(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+  });
+
+  function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 14) { // 15 minutes
+        logout();
+    }
+  }
+
 document.querySelector('#logout').addEventListener('click', logout);
